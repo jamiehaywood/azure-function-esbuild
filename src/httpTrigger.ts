ac0c6f035738065a692217e3c0832c1b4d305547
Jamie <jamie.haywood@gmail.com>
2022-10-22 13:19:03 +0100

    chore: refactor

--- src/httpTrigger.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpRequest, InvocationContext } from "@azure/functions";
-
-export default async (context: InvocationContext, request: HttpRequest) => {
-  context.log(`Http function processed request for url "${request.url}"`);
-
-  const name = request.query.get("name") || (await request.text()) || "world";
-
-  return { body: `Hello, ${name}!` };
-};
--- src/timerTrigger.ts
+++
@@ -1,6 +0,0 @@
-import type { InvocationContext, Timer } from "@azure/functions";
-
-export default async (context: InvocationContext, myTimer: Timer): Promise<void> => {
-  console.log("Hello");
-  context.log("Timer function processed request.");
-};
