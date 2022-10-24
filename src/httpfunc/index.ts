65da4e50fa634fcd3b72c286e641299ef45b1048
Jamie <jamie.haywood@gmail.com>
2022-10-24 23:54:30 +0100

    feat: update function with unix readout

--- src/httpfunc/index.ts
+++ src/httpfunc/index.ts
@@ -1,11 +1,11 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
-
+import Dayjs from "dayjs";
 export default async (context: Context, req: HttpRequest): Promise<void> => {
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
   const responseMessage = name
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    : `The time is ${Dayjs().unix()} UNIX time`;
 
   context.res = {
     // status: 200, /* Defaults to 200 */
