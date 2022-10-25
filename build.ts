c9c7342046db641b2c1dd0dc806d30fdda595777
Jamie <jamie.haywood@gmail.com>
2022-10-25 23:40:35 +0100

    refactor: tidy up build script

--- build.ts
+++ build.ts
@@ -4,34 +4,36 @@ import { copy } from "esbuild-plugin-copy";
 import { readdir, mkdir, readFile } from "fs/promises";
 import { rmSync } from "fs";
 import path from "path";
+
 async function build() {
   const sourcemap = !!process.argv[2];
-
+  const nvmrcVersion = await readFile(".nvmrc", { encoding: "utf-8" });
+  const sourceFolder = "src";
   const buildDir = "dist";
 
   rmSync(buildDir, { recursive: true, force: true });
   await mkdir(buildDir).catch(() => {}); // catch and do nothing if dir already exists
 
-  const getDirectories = async (source) =>
+  const getDirectories = async (source: string) =>
     (await readdir(source, { withFileTypes: true }))
       .filter((dirent) => dirent.isDirectory())
       .map((dirent) => dirent.name);
 
-  const srcDirs = await getDirectories("./src");
+  const srcDirs = await getDirectories(sourceFolder);
 
   for (const directory of srcDirs) {
-    const outdir = path.join(buildDir, `/${directory}`);
+    const outdir = path.join(buildDir, directory);
 
     await mkdir(outdir);
 
     await esbuild.build({
-      entryPoints: [path.join("./src", directory, "/index.ts")],
+      entryPoints: [path.join(sourceFolder, directory, "index.ts")],
       outdir,
       bundle: true,
       minify: true,
       platform: "node",
       treeShaking: true,
-      target: [`node${await readFile(".nvmrc", { encoding: "utf-8" })}`],
+      target: [`node${nvmrcVersion}`],
       sourcemap,
       watch: false,
       plugins: [
@@ -39,7 +41,7 @@ async function build() {
           copyOnStart: true,
           assets: [
             {
-              from: [`src/${directory}/function.json`],
+              from: [path.join(sourceFolder, directory, "function.json")],
               to: ["."],
             },
           ],
@@ -49,12 +51,3 @@ async function build() {
   }
 }
 build();
-// esbuild.build({
-//   entryPoints: ["./src/**"],
-//   outdir: "lib",
-//   bundle: true,
-//   minify: true,
-//   sourcemap: false,
-//   watch: false,
-//   //   plugins: [copyStaticFiles()],
-// });
