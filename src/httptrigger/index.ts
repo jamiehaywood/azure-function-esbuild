eefe2329756706e17230745c8ff351f126209a05
Jamie <jamie.haywood@gmail.com>
2022-10-25 23:43:43 +0100

    refactor: remove extraneous func & rename to httptrigger

--- src/httptrigger/index.ts
+++ src/httptrigger/index.ts
@@ -1,5 +1,6 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { Context, HttpRequest } from "@azure/functions";
 import Dayjs from "dayjs";
+
 export default async (context: Context, req: HttpRequest): Promise<void> => {
   context.log("HTTP trigger function processed a request.");
   const name = req.query.name || (req.body && req.body.name);
